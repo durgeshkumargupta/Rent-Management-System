c8837256df260c61760b6c61b0eb6dbc77c2bd6b
durgeshkumargupta <47785309+durgeshkumargupta@users.noreply.github.com>
2019-04-23 15:00:31 +0530

    Update Personal_Detalis.java

--- Personal_Detalis.java
+++ Personal_Detalis.java
@@ -342,7 +342,7 @@ public class Personal_Detalis extends javax.swing.JFrame {
            }
         }catch(Exception e)
         {
-            JOptionPane.showMessageDialog(null,e);
+         //   JOptionPane.showMessageDialog(null,e);
         }finally{
             try{
                 rs.close();
